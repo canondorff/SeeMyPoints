0cd5bbaa7e08110de49b650f84b8940b43a7b7d4
FisaroJuju <julien.ejupi@gmail.com>
2024-11-18 14:53:16 +0100

    Update UI

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -4,35 +4,12 @@ using System.Windows.Documents;
 
 namespace SeeMyPoints;
 
-public class EquipeListe
-{
-    public string NomEquipe { get; set; }
-    public string Score { get; set; }
-
-    public EquipeListe(string nomEquipe, string score)
-    {
-        NomEquipe = nomEquipe;
-        Score = score;
-    }
-}
-
 public partial class Dashboard : Page
 {
-    // Déclarez la liste sous forme d'ObservableCollection pour la mise à jour dynamique
-    public ObservableCollection<EquipeListe> EquipesListes { get; set; }
-
     public Dashboard()
     {
         InitializeComponent();
         LoadEleves();
-        // Remplissez la liste avec des données d'exemple
-        EquipesListes = new ObservableCollection<EquipeListe>
-        {
-            new EquipeListe("Equipe 1", "15"),
-            new EquipeListe("Equipe 2", "14"),
-            new EquipeListe("Equipe 3", "-"),
-        };
-
         // Assigner la liste au contexte de données pour le DataGrid
         DataContext = this;
     }
@@ -44,18 +21,5 @@ public partial class Dashboard : Page
     
         // Liaison de la liste d'élèves à la source d'éléments du DataGrid
         elevesDataGrid.ItemsSource = eleves;
-    }
-
-    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        ComboBox comboBox = sender as ComboBox;
-        EquipeListe selectedTeam = comboBox.SelectedItem as EquipeListe;
-
-        // Si vous souhaitez afficher des informations supplémentaires sur l'équipe sélectionnée
-        if (selectedTeam != null)
-        {
-            // Exemple : Affichage dans la console
-            Console.WriteLine($"Equipe sélectionnée : {selectedTeam.NomEquipe}, Score : {selectedTeam.Score}");
-        }
     }
 }
