83a3f8574d17c62b24dc290cc27dcb734363dea0
canondorff <canondorfchaine@gmail.com>
2024-11-22 11:53:51 +0100

    Import system

--- Leaderboard.xaml.cs
+++ Leaderboard.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using Microsoft.Win32;
 
 namespace SeeMyPoints;
 
@@ -11,13 +13,34 @@ public partial class Leaderboard : Page
         InitializeComponent();
     }
     
-    private void OpenPopup_Click(object sender, RoutedEventArgs e)
+    private void Import_Click(object sender, RoutedEventArgs e)
     {
-        MyPopup.IsOpen = true;
-    }
+        OpenFileDialog openFileDialog = new OpenFileDialog
+        {
+            Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+        };
 
-    private void ClosePopup_Click(object sender, RoutedEventArgs e)
+        if (openFileDialog.ShowDialog() == true)
+        {
+            string filePath = openFileDialog.FileName;
+        }
+    }
+    
+    private void ImportCsvToDatabase(string filePath)
     {
-        MyPopup.IsOpen = false;
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] values = line.Split(',');
+
+                if (values.Length >= 2)
+                {
+                    Eleve eleve = new Eleve(values[0], values[1]);
+                    ADOEleve.insertEleve(eleve);
+                }
+            }
+        }
     }
 }
